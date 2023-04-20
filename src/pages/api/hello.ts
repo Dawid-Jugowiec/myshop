42a50e84ed36481c6e7fe05812e00a72d53289a6
Dawid Jugowiec <d.j.w.jugowiec@gmail.com>
2023-04-21 00:20:24 +0200

    Poprawa pliku hello

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}})
+  res.status(200).json({name: "John Doe"})
 }
