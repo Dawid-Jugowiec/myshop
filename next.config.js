d18c3b9a85ec1c1bc10094d63fd0de2b9bf0e9f6
Dawid Jugowiec <d.j.w.jugowiec@gmail.com>
2023-04-24 20:32:53 +0200

    Usuniecie wpisu z next.config.

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
-    domains: [
-      'fakestoreapi.com',
-      'picsum.photos',
-      'myshop-hi4i8cgx7-dawid-jugowiec.vercel.app',
-    ],
+    domains: ['fakestoreapi.com', 'picsum.photos'],
     formats: ['image/avif', 'image/webp'],
   },
 };
